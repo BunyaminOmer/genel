07af5fffa93e52dd8b542bd32cc729ced7c1ea0a
BunyaminOmer <bunyaminomerkarakus@protonmail.com>
2023-09-23 18:44:59 +0300

    first commit

--- new.py
+++ new.py
@@ -1,3 +1,3 @@
 import requests
-r = requests.get("https://www.w3schools.com")
-print(f"Response [{r.status_code}]")
+status = requests.get("https://www.w3schools.com")
+print(status.content)
--- test.py
+++ test.py
@@ -1,63 +1,26 @@
 import requests
 
 class TargetSite:
-    def __init__(self, url):
-        self.url = url
+   def __init__(self, url):
+    self.url = url
 
-    @property
-    def url_bilgisi(self):
-        return self.url
-
-    @property
-    def request_method(self):
-        return 'GET'
-
+   @property
+   def url_bilgisi(self):
+    return self.url
+   
 class RequestService:
     def __init__(self, urller):
-        self.urller = urller
+      self.urller = urller
 
-    def istekleri_yap(self):
-        for url in self.urller:
-            target_site = TargetSite(url)
-            response = requests.get(target_site.url_bilgisi)
-            
-            print(f"Hedef Site: {target_site.url_bilgisi}")
-            print(f"Request Method: {target_site.request_method}")
-            print(f"Yanıt İçeriği:\n{response.text}\n")
+    def bilgileri_goster(self):
+      for s in self.urller:
+        response = requests.get(s.url)
+        print(response)
 
-# Kullanım örneği:
-urller = ("https://www.w3schools.com", "https://www.deneme.com")
-request_service = RequestService(urller)
-request_service.istekleri_yap()
+url_1 = TargetSite("https://udemy.com")
+url_2 = TargetSite("https://facebook.com")
+url_3 = TargetSite("https://w3schools.com")
 
-import requests
-
-class TargetSite:
-    def __init__(self, url):
-        self.url = url
-
-    @property
-    def url_bilgisi(self):
-        return self.url
-
-    @property
-    def request_method(self):
-        return 'GET'
-
-class RequestService:
-    def __init__(self, urller):
-        self.urller = urller
-
-    def ekrana_yazdir(self):
-            print(f"Hedef Site: {TargetSite.url_bilgisi}")
-            print(f"Request Method: {TargetSite.request_method}")
-
-    def bilgileri_yazdir(self):
-        for url in self.urller:
-            target_site = TargetSite(url)
-            istek = requests.get(TargetSite.url_bilgisi)
-
-t = TargetSite("https://www.w3schools.com")
-t2 = TargetSite("https://www.deneme.com")
-list = [t, t2]
-lis = RequestService(list)
+urller = [url_1, url_2, url_3]
+istek_at = RequestService(urller)
+istek_at.bilgileri_goster()
